d1a82b7e13e4d6ed154f9f736e1f84cc2fbd9968
priyanshawasthi98-cmyk <priyanshawasthi98@gmail.com>
2026-09-12 13:47:27 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='krishisathi-v1';const A=['./','./index.html','./manifest.json','./sw.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
